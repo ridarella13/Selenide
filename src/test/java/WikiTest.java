b82bd1e5987bd4e4d867e607fb7abfc372be3908
Tati <ridarella13@gmail.com>
2022-12-25 14:35:49 +0300

    add WikiTest

---
+++ src/test/java/WikiTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WikiTest {
+
+    @Test
+void WikiSeach () {
+//Откройте страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+        //Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+        $("#wiki-pages-box button").click();
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        // Откройте страницу SoftAssertions
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        // проверьте что внутри есть пример кода для JUnit5
+       $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+        sleep(10000);
+
+}
+
+}
