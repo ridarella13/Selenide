492c196a77ad0a30fd10831a533b5f8180a1a3ce
Tati <ridarella13@gmail.com>
2022-12-27 13:25:47 +0300

    added DragAndDropTest,EnterpriseTest

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+public class DragAndDropTest {
+    @Test
+    void successfulDragAndDrop() {
+        //Откройте https://the-internet.herokuapp.com/drag_and_drop
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //Перенесите прямоугольник А на место В
+        $("#column-a").dragAndDropTo($("#column-b"));
+        //actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(300, 300).release().perform(); //не работает
+        //Проверьте, что прямоугольники действительно поменялись
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
+        }
+}
---
+++ src/test/java/EnterpriseTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class EnterpriseTest {
+    @Test
+    void successfullenterprise() {
+        //Открыть страницу GitHub;
+        open("https://github.com/");
+        //Выбрать в навигационном меню Solutions;
+        $(byText("Solutions")).hover();
+        //В раскрывшемся списке выбрать Enterprise;
+        $(byText("Enterprise")).click();
+        //Убедиться, что открылась необходимая страница;
+        $(".h1-mktg").shouldHave(text("Build like the best"));
+    }
+}
+
